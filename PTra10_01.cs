f355ec02d43df2dd234437e916300031f7c6d186
TKBTK48 <66378115+TKBTK48@users.noreply.github.com>
2020-06-20 17:56:02 +0900

    Create PTra10_01.cs

---
+++ PTra10_01.cs
@@ -0,0 +1,27 @@
+/*
+ * PTra10_01.cs
+ *   作成	Rhizome
+ *------------------------------------------------------------
+ * Copyright(c) Rhizome Inc. All Rights Reserved.
+ */
+
+using System;
+
+public class PTra10_01
+{
+    public static void Main(string[] args)
+    {
+        // TODO User型変数 user を宣言してください。
+
+        // TODO 変数 user に Userインスタンスを代入してください。
+
+        // TODO 変数 userのidフィールドに 1000 を代入してください。
+
+        // TODO 変数 userのnameフィールドに 山田太郎 を代入してください。
+
+        // TODO 変数 userの idフィールド を出力してください。
+
+        // TODO 変数 userの nameフィールド を出力してください。
+
+    }
+}
