8a42c389dc99ee03357d58f61d9576b4438f75ed
TKBTK48 <66378115+TKBTK48@users.noreply.github.com>
2020-06-20 17:58:27 +0900

    Create PTra10_04.cs

---
+++ PTra10_04.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * PTra10_04.cs
+ *   作成	Rhizome
+ *------------------------------------------------------------
+ * Copyright(c) Rhizome Inc. All Rights Reserved.
+ */
+
+/*
+ * TODO 以下の仕様のクラスを作成してください
+ * （このファイルとは別に、新しくcsファイルを作成してください）
+ *
+ * クラス名
+ * 		Calculation
+ * フィールド
+ * 		baseNum		:	int型
+ * メソッド
+ * 		戻り値(なし)、メソッド名(Add)、引数(int num)
+ * 		フィールドbaseNum に引数num を加える
+ */
+using System;
+
+public class PTra10_04
+{
+    public static void Main(string[] args)
+    {
+        // 以下の問題はCalculationクラスを作成してから解いてください
+
+        // TODO Calculation型変数 cal をCalculation型のインスタンスで初期化してください。
+
+        // TODO 変数 calの baseNumフィールドに 100 を代入してください。
+
+        // TODO 変数 calの Addメソッドを引数に100を渡して呼び出してください。
+
+        // TODO 変数 calの baseNumフィールドを出力してください。
+        // ※ 200 と出力されれば正解
+
+    }
+}
