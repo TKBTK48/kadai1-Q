498b4374e6ca91f302fad09f2f644629e98d98e1
TKBTK48 <66378115+TKBTK48@users.noreply.github.com>
2020-06-20 18:00:38 +0900

    Create PTra10_06.cs

---
+++ PTra10_06.cs
@@ -0,0 +1,33 @@
+/*
+ * PTra10_06.cs
+ *   作成	Rhizome
+ *------------------------------------------------------------
+ * Copyright(c) Rhizome Inc. All Rights Reserved.
+ */
+
+using System;
+
+public class PTra10_06
+{
+    /**
+  	 * @param args
+  	 */
+    public static void Main(string[] args)
+    {
+        // TODO User型の変数が3つ入る配列userListを宣言・確保してください
+
+        // TODO User型変数のインスタンスをuser1, user2, user3という名前で3つ作成してください。
+
+        /*
+    	 *  TODO 作成した3つのインスタンスのフィールドにそれぞれ以下を代入してください。
+    	 *  user1	: idフィールドに 1、nameフィールドに 一郎
+    	 *  user2	: idフィールドに 2、nameフィールドに 二郎
+    	 *  user3	: idフィールドに 3、nameフィールドに 三郎
+    	 */
+
+        // TODO userListに作成したuser1, user2, user3を追加してください。
+
+        // TODO 繰り返しを使ってuserListの中身を順番に取り出してPrintProfileメソッドを実行してください。
+
+    }
+}
