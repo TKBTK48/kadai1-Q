b3706875bb5a7621efbdda02dc3ebc3dd1c612e6
TKBTK48 <66378115+TKBTK48@users.noreply.github.com>
2020-06-20 18:02:45 +0900

    Create User.cs

---
+++ User.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * User.cs
+ *   作成	Rhizome
+ *------------------------------------------------------------
+ * Copyright(c) Rhizome Inc. All Rights Reserved.
+ */
+
+using System;
+
+public class User
+{
+    internal int id;
+    internal string name;
+
+    /**
+  	 * IDと名前を出力する挨拶メソッド
+  	 */
+    internal void PrintProfile()
+    {
+        Console.WriteLine("ID" + this.id + ":" + this.name);
+    }
+}
