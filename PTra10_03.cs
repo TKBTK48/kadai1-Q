8ae09d3bd1e980cb5ccfc66f3bed2d02cf3b68ac
TKBTK48 <66378115+TKBTK48@users.noreply.github.com>
2020-06-20 17:57:45 +0900

    Create PTra10_03.cs

---
+++ PTra10_03.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * PTra10_03.cs
+ *   作成	Rhizome
+ *------------------------------------------------------------
+ * Copyright(c) Rhizome Inc. All Rights Reserved.
+ */
+
+/*
+ * TODO 以下の仕様のクラスを作成してください
+ * （このファイルとは別に、新しくcsファイルを作成してください）
+ *
+ * クラス名
+ * 		Student
+ * フィールド
+ * 		studentId	:	int型
+ * 		name		:	string型
+ * 		mail		:	string型
+ * 		password	:	string型
+ * メソッド
+ * 		なし
+ */
+
+using System;
+
+public class PTra10_03
+{
+    public static void Main(string[] args)
+    {
+        // 以下の問題はStudentクラスを作成してから解いてください
+
+        // TODO Student型変数 student をStudent型のインスタンスで初期化してください。
+
+        // TODO 変数 studentの studentIdフィールドに 10 を代入してください。
+
+        // TODO 変数 studentの nameフィールドに 田中二郎 を代入してください。
+
+        // TODO 変数 studentの mailフィールドに test@mail.com を代入してください。
+
+        // TODO 変数 studentの passwordフィールドに pass を代入してください。
+
+        // TODO studentの全てのフィールド(studentId, name, mail, password)それぞれを出力してください
+
+    }
+}
